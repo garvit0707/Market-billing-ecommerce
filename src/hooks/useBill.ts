53d4747781350688477287b1db4e796d9670e4ee
Garvit Sharma <161028881+garvit0707@users.noreply.github.com>
2026-04-17 19:07:29 +0530

    root bills and redux setup

---
+++ src/hooks/useBill.ts
@@ -0,0 +1,12 @@
+import { useMemo } from 'react';
+import { useAppSelector } from './redux';
+import { calculateBill } from '../utils/billing';
+import { BillBreakdown } from '../types';
+
+export function useBill(): BillBreakdown {
+  const items = useAppSelector((state) => state.cart.items);
+  
+  const bill = useMemo(() => calculateBill(items), [items]);
+
+  return bill;
+}
